3e312e26fc96b63a38866d04b0639450bdb94e7c
noahniemeijer <noahniemeijer0603@gmail.com>
2026-06-28 17:57:49 +0200

    Malfunctioning version

--- mcloud/src/routes/+layout.svelte
+++ mcloud/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
   import '../layout.css';
+  if (typeof window !== 'undefined') {
+    window.onerror = function(msg, source, line, col, err) {
+      document.body.innerHTML = '<pre style="color:white;padding:20px;font-size:14px">' + msg + '\n' + source + ':' + line + ':' + col + '</pre>'
+    }
+  }
+
+
 </script>
 
 <div class="bg-bg-primary w-screen h-screen">
